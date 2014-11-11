3e0615895314323a0c4a93e9f859e1aa6b96f885
Sam Kantor <sam.kantor@gmail.com>
2014-11-10 21:56:28 -0500

    Added login method

--- GMessenger/GMessenger/Controllers/Test1Controller.cs
+++ GMessenger/GMessenger/Controllers/Test1Controller.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using GMessenger.Models;
+
 namespace GMessenger.Controllers
 {
     public class Test1Controller : ApiController
@@ -16,44 +18,107 @@ namespace GMessenger.Controllers
         }
 
         // GET: api/Test1/5
-        [System.Web.Http.ActionName("GetLastCount")]
-        public string GetLastCount(string id)
+        [System.Web.Http.ActionName("GetGroup")]
+        public PasswordModel GetGroup(string id)
         {
-            // return "10";
+            PasswordModel pm = new PasswordModel();
+            pm.isError = true;
             try
             {
+                string[] info = id.Split(',');
+
+                string group_name = info[0];
+                string group_password = info[1];
+
                 gconnectdbEntities db = new gconnectdbEntities();
            
 
-                var a = (from e in db.Messages
+                var a = (from e in db.Groups
+                         where e.group_name.ToUpper().Equals(group_name.ToUpper()) && e.group_user_pass.ToUpper().Equals(group_password.ToUpper())
                          select e).ToList();
 
-                string txtsummary = "";
 
-                foreach (var m in a)
+                if (a.Count > 0)
                 {
-                    txtsummary += m.text + ";";
+                    pm.group_id = a.FirstOrDefault().group_id;
+                    pm.isError = false;
                 }
 
-                return txtsummary;
+                return pm;
             }
             catch (Exception ex)
             {
-                return ex.Message;
+                return pm;
             }
         }
 
         [System.Web.Http.ActionName("GetMessage")]
-        public string GetMessage(string id)
+        public MessageModel GetMessage(string id)
         {
             try
             {
                 string[] info = id.Split(',');
-                return "This is a content of a sample message for group" + info[0] + "and id" + info[1];
+
+                string groupNo = "";
+                string messageNo = "";
+                int group2 = -1;
+                int message = -1;
+
+                if(info.Count() > 0)
+                {
+                    groupNo = info[0];
+                    group2 = Int32.Parse(groupNo);
+               
+                }
+                if(info.Count() > 1)
+                {
+                    messageNo = info[1];
+                    message = Int32.Parse(messageNo);
+                }
+                
+                gconnectdbEntities db = new gconnectdbEntities();
+
+
+
+                var msgs = (from e in db.Messages
+                            where e.group_id.Equals(group2)
+                             select e).ToList();
+
+                string messageText = "";
+
+                if (msgs.Count > 0)
+                {
+                    if (message == -1)
+                    {
+                        messageText = msgs.Last().text;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            messageText = msgs.FirstOrDefault(m => m.message_id.Equals(message)).text;
+                        }
+                        catch(Exception)
+                        {
+                            messageText = "This message doesn't exist";
+                        }
+                    }
+                }
+
+                MessageModel msgModel = new MessageModel();
+                msgModel.text = messageText;
+                msgModel.lastMessage = msgs.Count;
+                msgModel.isError = false;
+
+                return msgModel;
             }
             catch (Exception ex)
             {
-                return "You sent:" + id + " an exception happened " + ex.Message;
+                MessageModel msgModel = new MessageModel();
+                msgModel.isError = true;
+                msgModel.lastMessage = 0;
+                msgModel.text = "You sent:" + id + " an exception happened " + ex.Message;
+                return msgModel;
             }
         }
 
---
+++ GMessenger/GMessenger/Models/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GMessenger.Models
+{
+    public class MessageModel
+    {
+        public string text;
+        public int lastMessage;
+        public bool isError;
+
+        public MessageModel()
+        {
+            text = "";
+            lastMessage = 0;
+            isError = false;
+        }
+    }
+
+    public class PasswordModel
+    {
+        public int group_id;
+        public bool isError;
+
+        public PasswordModel()
+        {
+            group_id = -1;
+            isError = false;
+        }
+    }
+}
